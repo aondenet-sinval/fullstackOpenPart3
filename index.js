7eb7a0614af5e5bb389bf7aefbbb146093d70eff
aondenet-sinval <sinvalsatv@gmail.com>
2022-10-18 19:54:17 -0300

    end part3 eslint

--- index.js
+++ index.js
@@ -2,103 +2,101 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
-const mongoose = require('mongoose')
 
 const Person = require('./models/person')
-    app.use(cors())
-    app.use(express.static('build'))
-    app.use(express.json())
-    app.get('/', (request, response)=>{
-      response.send('<h1>Hello world</h1>')
-    })
-    app.get('/api/persons', (request, response)=>{
-      Person.find({}).then(persons => {
-        response.json(persons)
-      })
-    })
-    app.get('/info', (request, response)=>{
-      Person.find({}).then(persons => {
-        response.send(`Phonebook has info for ${persons.length} person. <br /> ${Date()}.`)
-      })
-
-    })
-    app.get('/api/persons/:id', (request, response, next) => {
-      Person.findById(request.params.id)
-        .then(person =>{
-          if (person) {
-            response.json(person)
-          }else {
-            response.status(404).end()
-          }
-        })
-        .catch(error => next(error))
-    })
-    //error:
-    const errorHandler = (error, request, response, next ) => {
-      // console.error(error.message)
-      if(error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
-      }
-      if(error.name === 'ValidationError') {
-        return response.status(400).send({ error: `${error}` })
-      }
-
-      next(error)
-    }
-    app.use(errorHandler)
-    morgan.token('valueName', function(req, res) {
-      return '{"name": "' + req.body.name + '" "number": "'
-                + req.body.number + '"}';
-    });
-    app.use(morgan(':method :url :status :res[content-length] :response-time ms :valueName'))
-    //post
-    app.post('/api/persons', (request, response, next) => {
-      const body = request.body
-
-        //Verificando se o campo name foi preenchido
-        if (!body.name) {
-          return response.status(400).json({
-            error: 'faltou name'
-          })
-        }
-
-        const person = new Person({
-          name: body.name,
-          number: body.number,
-        })
-        person.save().then(result => {
-          // console.log(`add ${person.name} number ${person.number} to phonebook!`)
-          response.json(person)
-        })
-        .catch(error => next(error))
-
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+app.get('/', (request, response) => {
+  response.send('<h1>Hello world</h1>')
 })
-    //put
-  app.put('/api/persons/:id', (request, response, next) =>{
-    const body = request.body
-    const  person = {
-      number: body.number,
-    }
-    // validator: runValidators: true, context: 'query'
-    Person.findByIdAndUpdate(request.params.id, person, { new: true, runValidators: true, context: 'query' })
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
+})
+app.get('/info', (request, response) => {
+  Person.find( { } ).then(persons => {
+    response.send(`Phonebook has info for ${persons.length} person. <br /> ${Date()}.`)
+  } )
+
+} )
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    } )
+    .catch(error => next(error))
+})
+//error:
+const errorHandler = (error, request, response, next ) => {
+  // console.error(error.message)
+  if(error.name === 'CastError') {
+    return response.status(400).send( { error: 'malformatted id' } )
+  }
+  if(error.name === 'ValidationError') {
+    return response.status(400).send( { error: `${error}` } )
+  }
+
+  next(error)
+}
+app.use(errorHandler)
+morgan.token('valueName', function(req) {
+  return '{"name": "' + req.body.name + '" "number": "'
+            + req.body.number + '"}'
+})
+app.use(morgan(':method :url :status :res[content-length] :response-time ms :valueName'))
+//post
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
+
+  //Verificando se o campo name foi preenchido
+  if (!body.name) {
+    return response.status(400).json( {
+      error: 'faltou name'
+    } )
+  }
+
+  const person = new Person( {
+    name: body.name,
+    number: body.number,
+  } )
+  person.save().then(result => {
+    response.json(result)
+  } )
+    .catch(error => next(error))
+})
+//put
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+  const  person = {
+    number: body.number,
+  }
+  // validator: runValidators: true, context: 'query'
+  Person.findByIdAndUpdate(request.params.id, person, { new: true, runValidators: true, context: 'query' })
     .then(updatedPerson => {
       response.json(updatedPerson)
-      })
-      .catch(error => next(error))
-})
-  // app.use(errorHandler)
-    //delete
-    app.delete('/api/persons/:id', (request, response, next) => {
-      Person.findByIdAndRemove(request.params.id)
-        .then(result => {
-          response.status(204).end()
-        })
-        .catch(error => next(error))
+    } )
+    .catch(error => next(error))
+} )
+// app.use(errorHandler)
+//delete
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.json(result)
+      response.status(204).end()
     })
+    .catch(error => next(error))
+} )
 
 app.use(errorHandler)
 const PORT = process.env.PORT || 3001
 
-    app.listen(PORT,()=>{
-      console.log(`Server express running on port: ${PORT}.`);
-    })
+app.listen(PORT,() => {
+  console.log(`Server express running on port: ${PORT}.`)
+} )
--- models/person.js
+++ models/person.js
@@ -1,27 +1,25 @@
 const mongoose = require('mongoose')
-const uniqueValidator = require('mongoose-unique-validator');
+const uniqueValidator = require('mongoose-unique-validator')
 //Conexão local:
-  // const url = 'mongodb://localhost:27017/phonebook'
+const url = 'mongodb://localhost:27017/phonebook'
 //Conexão MongoDB Atlas:
-const url = process.env.MONGODB_URI
+// const url = process.env.MONGODB_URI
 
 
 mongoose.connect(url)
 
-const personSchema = new mongoose.Schema({
-  name: { type: String, minlength: 3, unique: true },
-  number: { type: String, minlength: 8 },
+const personSchema = new mongoose.Schema( {
+  name: { type: String, minlength: 3, unique: true } ,
+  number: { type: String, minlength: 8 } ,
 })
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-  returnedObject.id = returnedObject._id.toString()
-  delete returnedObject._id
-  delete returnedObject.__v
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   }
 })
-personSchema.plugin(uniqueValidator);
-
-const Person = mongoose.model('Person', personSchema)
+personSchema.plugin(uniqueValidator)
 
 module.exports = mongoose.model('Person', personSchema)
